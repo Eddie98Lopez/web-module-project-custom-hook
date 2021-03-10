cf41b6c41ce11362110704efd51adf35e0a51574
Eduardo Lopez <ed98lopez@gmail.com>
2021-03-10 15:08:08 -0800

    figured it out....had to rewatch some stuff and review guided project code but I figured it out

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
+import {useDark} from './hooks/useDark'
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 
 import "./styles.scss";
 
+
 const App = () => {
   const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+  const [dark,setDark]=useDark(false)
 
   useEffect(() => {
     axios
@@ -20,8 +22,8 @@ const App = () => {
       .catch(err => console.log(err));
   }, []);
   return (
-    <div className={darkMode ? "dark-mode App" : "App"}>
-      <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+    <div className={dark ? "dark-mode App" : "App"}>
+      <Navbar darkMode={dark} setDarkMode={setDark} />
       <Charts coinData={coinData} />
     </div>
   );
